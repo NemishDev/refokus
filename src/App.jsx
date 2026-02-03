a0af6a3bb385a5eb0f25f79e3d32002fa29bd9ae
Nemish Mogra <mogranemish@gmail.com>
2026-02-03 23:13:36 +0530

    loader commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+    return (
+  <div className="w-full h-screen bg-zinc-900 text-white font-['satoshi_variable']">
+            <Navbar />
+           
+        </div>
+    )
+}
+
+export default App;
