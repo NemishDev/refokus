70f47c59277b657d0ee9c0ab1c6ed86a003ad65f
Nemish Mogra <mogranemish@gmail.com>
2026-02-04 15:45:02 +0530

    product part done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react"
 import Navbar from "./Components/Navbar"
 import Work from "./Components/Work";
 import Stripes from "./Components/Stripes";
+import Products from "./Components/Products";
 
 const App = () => {
     return (
-  <div className="w-full h-screen bg-zinc-900 text-white font-['satoshi_variable']">
+  <div className="w-full h-screen  text-white font-['satoshi_variable']">
             <Navbar />
             <Work />
             <Stripes />
+            <Products />
            
         </div>
     )
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React from "react"
 import { IoIosReturnRight } from "react-icons/io"
 
-function Button(){
-  return(
-    <div className="min-w-40 px-4
-    py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between " >
-<span className="text-sm font-medium"> Get Started</span>
-        <IoIosReturnRight />
-    </div>
-  )
+const Button = ({ title = "Get Started" }) => {
+    return (
+        <div className="w-[130px] px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between">
+            <span className="text-sm font-medium">{title}</span>
+            <IoIosReturnRight />
+        </div>
+    )
 }
 
-export default Button;
+export default Button
