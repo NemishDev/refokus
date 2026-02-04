d193c1cf4a62c96d606389e5370f3176976e20fe
Nemish Mogra <mogranemish@gmail.com>
2026-02-04 22:45:58 +0530

    card component completed with framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Work from "./Components/Work";
 import Stripes from "./Components/Stripes";
 import Products from "./Components/Products";
 import Marquees from "./Components/Marquees";
+import Cards from "./Components/Cards";
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
             <Stripes />
             <Products />
             <Marquees />
+            <Cards />
            
         </div>
     )
